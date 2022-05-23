216e11fd890627a5c75449a38d14420151a17039
Vsevolod Fedoseev <vsevolod18031999@gmail.com>
2022-05-23 15:18:45 +0200

    Component Header NavBar Profile

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Header from './components/Header'
 import Navbar from './components/Navbar'
+import Profile from './components/Profile'
 
 
 function App() {
@@ -9,23 +10,7 @@ function App() {
    <div className='app-wrapper'>
      <Header />
      <Navbar />
-     <div className='content'>
-          <img src="https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg" alt=""/>
-     </div>
-       <div>
-           ava + description
-       </div>
-       <div>
-           My posts
-           <div>
-               new post
-           </div>
-           <div>
-               <div>post 1</div>
-               <div>post 2</div>
-           </div>
-
-       </div>
+     <Profile />
 
    </div>
   );
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+
+const Profile = () => {
+    return <div className='content'>
+     <div>
+        <img src="https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg" alt=""/>
+    </div>
+    <div>
+        ava + description
+    </div>
+    <div>
+        My posts
+        <div>
+            new post
+        </div>
+        <div>
+            <div>post 1</div>
+            <div>post 2</div>
+        </div>
+
+    </div>
+    </div>
+}
+export default Profile;
