d19cd9396f9237f35587c0425bdbff476eecc16b
Vsevolod Fedoseev <vsevolod18031999@gmail.com>
2022-05-24 12:26:17 +0200

    Component Header NavBar Profile NEXT DAY

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-
+import React from 'react';
 const Header = () => {
     return <header className='header'>
         <img src="https://cdn.logo.com/hotlink-ok/logo-social.png" alt=""/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-
+import React from 'react';
 const Navbar = () => {
     return <nav className='nav'>
         <div>
